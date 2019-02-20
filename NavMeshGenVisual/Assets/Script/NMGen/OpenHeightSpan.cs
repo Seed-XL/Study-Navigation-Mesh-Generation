1facdfce294a4d80fc120de848eba18ab0863f3e
jonbinliu <jonbinliu@hotmail.com>
2019-02-20 22:29:07 +0800

    答辩之后，继续看。。。

--- NavMeshGenVisual/Assets/Script/NMGen/OpenHeightSpan.cs
+++ NavMeshGenVisual/Assets/Script/NMGen/OpenHeightSpan.cs
@@ -47,5 +47,112 @@ namespace NMGen
             return mDistanceToBorder;  
         }
 
+        public int distanceToRegionCore()
+        {
+            return mDistanceToRegionCore; 
+        }
+
+        public int floor()
+        {
+            return mFloor;  
+        }
+
+        public OpenHeightSpan getNeighbor( int direction )
+        {
+            switch (direction)
+            {
+                case 0: return mNeighborConnection0;
+                case 1: return mNeighborConnection1;
+                case 2: return mNeighborConnection2;
+                case 3: return mNeighborConnection3;
+                default:
+                    return null; 
+            }
+
+        }
+
+        public int regionID()
+        {
+            return mRegionID; 
+        }
+
+        public void getDetailedRegionMap(ref int[] outData , int insertIndex)
+        {
+            for(int i = 0; i < 8; ++i)
+            {
+                outData[insertIndex + i] = NULL_REGION; 
+            }
+
+            OpenHeightSpan nSpan = null;
+            OpenHeightSpan nnSpan = null; 
+
+            for(int dir = 0; dir < 4; dir++)
+            {
+                nSpan = getNeighbor(dir); 
+                if( nSpan != null )
+                {
+                    outData[insertIndex + dir] = nSpan.regionID();
+                    nnSpan = nSpan.getNeighbor((dir + 1) & 0x3); 
+                    if( nnSpan != null )
+                    {
+                        outData[insertIndex + dir + 4] = nnSpan.regionID(); 
+                    }
+
+                    nnSpan = nSpan.getNeighbor((dir + 3) & 0x3); 
+                    if( nnSpan != null )
+                    {
+                        outData[insertIndex+((dir+3)&0x3)+4] = nnSpan.regionID(); 
+                    }
+                }
+            }
+
+        }
+
+
+        public int height()
+        {
+            return mHeight; 
+        }
+
+        public OpenHeightSpan next()
+        {
+            return mNext; 
+        }
+
+        public void setDistanceToBorder( int value )
+        {
+            mDistanceToBorder = Math.Max(value, 0); 
+        }
+
+        public void setDistanceToRegionCore(int value)
+        {
+            mDistanceToRegionCore = Math.Max(value, 0);  
+        }
+
+        public void setNeighbor(int direction , OpenHeightSpan neighbor)
+        {
+            switch(direction)
+            {
+                case 0: mNeighborConnection0 = neighbor; break;
+                case 1: mNeighborConnection0 = neighbor; break;
+                case 2: mNeighborConnection0 = neighbor; break;
+                case 3: mNeighborConnection0 = neighbor; break;
+            }
+        }
+
+        public void setNext(OpenHeightSpan value)
+        {
+            mNext = value; 
+        }
+
+        public void setRegionID(int value)
+        {
+            mRegionID = value; 
+        }
+
+        public string toString()
+        {
+            return string.Format("[OpenHeightSpan][toString]{0}|{1}|{2}",mFloor,mHeight,mRegionID); 
+        }
     }
 }
